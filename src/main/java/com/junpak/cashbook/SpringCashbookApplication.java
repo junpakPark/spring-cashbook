8fff348f054c8b4f630f6b4ed15e17cdb4450497
junpak <junpak.park@gmail.com>
2026-06-05 18:23:50 +0900

    chore: initialize spring cashbook project

---
+++ src/main/java/com/junpak/cashbook/SpringCashbookApplication.java
@@ -0,0 +1,13 @@
+package com.junpak.cashbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCashbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCashbookApplication.class, args);
+	}
+
+}
