fc6be72306ca07dc45233f2d382847c06c52d876
junpak <junpak.park@gmail.com>
2026-06-07 17:43:05 +0900

    feat: 분개 계정 검증 추가

---
+++ src/test/java/com/junpak/cashbook/domain/journal/JournalAccountsTest.java
@@ -0,0 +1,42 @@
+package com.junpak.cashbook.domain.journal;
+
+import static com.junpak.cashbook.domain.journal.Account.*;
+import static org.assertj.core.api.Assertions.*;
+
+import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.Test;
+
+@SuppressWarnings("NonAsciiCharacters")
+class JournalAccountsTest {
+
+	@Test
+	void 차변과_대변_계정을_생성한다() {
+		final JournalAccounts journalAccounts = new JournalAccounts(CASH, INCOME);
+
+		SoftAssertions.assertSoftly(softly -> {
+			softly.assertThat(journalAccounts.getDebit()).isEqualTo(CASH);
+			softly.assertThat(journalAccounts.getCredit()).isEqualTo(INCOME);
+		});
+	}
+
+	@Test
+	void 차변_계정은_필수이다() {
+		assertThatThrownBy(() -> new JournalAccounts(null, INCOME))
+			.isInstanceOf(NullPointerException.class)
+			.hasMessage("차변 계정이 누락되었습니다.");
+	}
+
+	@Test
+	void 대변_계정은_필수이다() {
+		assertThatThrownBy(() -> new JournalAccounts(CASH, null))
+			.isInstanceOf(NullPointerException.class)
+			.hasMessage("대변 계정이 누락되었습니다");
+	}
+
+	@Test
+	void 차변과_대변_계정은_같을_수_없다() {
+		assertThatThrownBy(() -> new JournalAccounts(CASH, CASH))
+			.isInstanceOf(IllegalArgumentException.class)
+			.hasMessage("차변과 대변 계정은 같을 수 없습니다.");
+	}
+}
