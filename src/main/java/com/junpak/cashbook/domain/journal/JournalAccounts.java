34c2535aa5349ee70686c87ae1319a0989439517
junpak <junpak.park@gmail.com>
2026-06-07 17:43:05 +0900

    feat: 분개 계정 검증 추가

--- src/main/java/com/junpak/cashbook/domain/journal/JournalAccounts.java
+++ src/main/java/com/junpak/cashbook/domain/journal/JournalAccounts.java
@@ -1,5 +1,7 @@
 package com.junpak.cashbook.domain.journal;
 
+import java.util.Objects;
+
 import jakarta.persistence.Embeddable;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
@@ -18,8 +20,17 @@ public class JournalAccounts {
 	private Account credit;
 
 	public JournalAccounts(final Account debit, final Account credit) {
+		validate(debit, credit);
 		this.debit = debit;
 		this.credit = credit;
+	}
+
+	private void validate(Account debit, Account credit) {
+		Objects.requireNonNull(debit, "차변 계정이 누락되었습니다.");
+		Objects.requireNonNull(credit, "대변 계정이 누락되었습니다");
+		if (debit == credit) {
+			throw new IllegalArgumentException("차변과 대변 계정은 같을 수 없습니다.");
+		}
 	}
 
 }
