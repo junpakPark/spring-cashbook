23776f85a458924f44dfe337adb36474931bd0f5
junpak <junpak.park@gmail.com>
2026-06-07 20:05:45 +0900

    test: 거래 API 테스트 구조 정리

--- src/test/java/com/junpak/cashbook/ui/ApiTest.java
+++ src/test/java/com/junpak/cashbook/ui/ApiTest.java
@@ -1,6 +1,7 @@
-package com.junpak.cashbook;
+package com.junpak.cashbook.ui;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.server.LocalServerPort;
@@ -10,11 +11,18 @@ import io.restassured.RestAssured;
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class ApiTest {
 
+	@Autowired
+	private DatabaseCleaner databaseCleaner;
+
 	@LocalServerPort
 	private int port;
 
 	@BeforeEach
 	void setUp() {
-		RestAssured.port = port;
+		if (RestAssured.port == RestAssured.UNDEFINED_PORT) {
+			RestAssured.port = port;
+		}
+		databaseCleaner.clear();
 	}
+
 }
