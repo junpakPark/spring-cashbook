f60d8c6ffb28d89e3c04c703880c841d6cb41986
junpak <junpak.park@gmail.com>
2026-06-08 21:04:43 +0900

    refactor: 재무상태 조회 반환 타입 변경

--- src/main/java/com/junpak/cashbook/application/dto/request/FinancialReportCondition.java
+++ src/main/java/com/junpak/cashbook/application/dto/request/FinancialReportCondition.java
@@ -4,6 +4,8 @@ import java.time.LocalDateTime;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
+import com.junpak.cashbook.domain.journal.Journal;
+
 public record FinancialReportCondition(
 	@DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
 	LocalDateTime until,
@@ -12,4 +14,8 @@ public record FinancialReportCondition(
 	@DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
 	LocalDateTime to
 ) {
+
+	public static FinancialReportCondition from(Journal journal) {
+		return new FinancialReportCondition(journal.getTransactionDate(), null, null);
+	}
 }
--- src/main/java/com/junpak/cashbook/presentation/FinancialReportController.java
+++ src/main/java/com/junpak/cashbook/presentation/FinancialReportController.java
@@ -9,6 +9,7 @@ import com.junpak.cashbook.application.dto.request.FinancialReportCondition;
 import com.junpak.cashbook.application.dto.response.FinancialPositionResponse;
 import com.junpak.cashbook.application.dto.response.ProfitAndLossResponse;
 import com.junpak.cashbook.application.dto.response.TrialBalanceResponse;
+import com.junpak.cashbook.domain.FinancialPosition;
 
 import lombok.RequiredArgsConstructor;
 
@@ -20,7 +21,9 @@ public class FinancialReportController {
 
 	@GetMapping("/financial-position")
 	public ResponseEntity<FinancialPositionResponse> getFinancialPosition(FinancialReportCondition condition) {
-		return ResponseEntity.ok(financialReportReader.getFinancialPosition(condition));
+		FinancialPosition financialPosition = financialReportReader.getFinancialPosition(condition);
+
+		return ResponseEntity.ok(FinancialPositionResponse.from(financialPosition));
 	}
 
 	@GetMapping("/profits-and-losses")
